34d08d25be5cf4a067477a25086a1e3df10ae6fa
Ryan Heupler & Jing Wait <student@epicodus.com>
2015-08-06 15:11:46 -0700

    should handle all amounts of change

--- lib/coin_counter.rb
+++ lib/coin_counter.rb
@@ -5,7 +5,7 @@ class Fixnum
     money = self
     quarters = 0
     dimes = 0
-    nickels = 0
+    nickles = 0
     pennies = 0
 
     until money.<(25)
@@ -13,15 +13,29 @@ class Fixnum
       quarters = quarters.+(1)
     end
 
-      quarter_change = quarters.to_s().concat(" Quarter(s)")
+      quarter_change = quarters.to_s().concat(" Quarter(s), ")
       grand_total.concat(quarter_change)
 
     until money.<(10)
       money = money.-(10)
       dimes = dimes.+(1)
     end
-      dime_change = dimes.to_s().concat(" Dime(s)")
+      dime_change = dimes.to_s().concat(" Dime(s), ")
       grand_total.concat(dime_change)
+
+    until money.<(5)
+      money = money.-(5)
+      nickles = nickles.+(1)
+    end
+      nickle_change = nickles.to_s().concat(" Nickle(s), ")
+      grand_total.concat(nickle_change)
+
+    until money.<(1)
+      money = money.-(1)
+      pennies = pennies.+(1)
+    end
+      pennies_change = pennies.to_s().concat(" Pennie(s)")
+      grand_total.concat(pennies_change)
   end
 end
 
--- spec/coin_counter_spec.rb
+++ spec/coin_counter_spec.rb
@@ -3,13 +3,19 @@ require("coin_counter")
 require("pry")
 
 describe("Fixnum#coin_counter") do
-  it ('will return correct string for single quarter') do
-    expect((25).coin_counter()).to(eq("1 Quarter(s), 0 Dime(s), 0 Nickles(s),
-    0 Pennie(s)"))
+  it ("will return correct string for single quarter") do
+    expect((25).coin_counter()).to(eq("1 Quarter(s), 0 Dime(s), 0 Nickle(s), 0 Pennie(s)"))
   end
 
   it ("will return string for correct number of dimes") do
-    expect((35).coin_counter()).to(eq("1 Quarter(s), 1 Dime(s), 0 Nickles(s),
-    0 Pennie(s)"))
+    expect((35).coin_counter()).to(eq("1 Quarter(s), 1 Dime(s), 0 Nickle(s), 0 Pennie(s)"))
+  end
+
+  it ("will return string for correct number of nickles") do
+    expect((40).coin_counter()).to(eq("1 Quarter(s), 1 Dime(s), 1 Nickle(s), 0 Pennie(s)"))
+  end
+
+  it ("will return string for correct number of pennies") do
+    expect((44).coin_counter()).to(eq("1 Quarter(s), 1 Dime(s), 1 Nickle(s), 4 Pennie(s)"))
   end
 end
