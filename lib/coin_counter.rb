ae8fd62ac8662f9b9202e79e6a483b16b2c1d36e
Ryan Heupler & Jing Wait <student@epicodus.com>
2015-08-06 14:14:19 -0700

    Rewrote spec, will handle any amount of quarters

--- lib/coin_counter.rb
+++ lib/coin_counter.rb
@@ -1,9 +1,53 @@
 class Fixnum
 
   define_method(:coin_counter) do
-    # change = [25, 10, 5, 1]
-    if self == 25
-      "1 Quarter"
+    grand_total = ""
+    money = self
+    quarters = 0
+    dimes = 0
+    nickels = 0
+    pennies = 0
+
+    until money.<(25)
+      money = money.-(25)
+      quarters = quarters.+(1)
     end
+      quarter_change=quarters.to_s().concat(" Quarter")
   end
 end
+
+
+
+
+
+
+    # change = [25, 10, 5, 1]
+    # change=self.join()
+    # amount=
+    # if self=1
+    #   change=self.join("cent")
+    # elsif self<=5
+    #   change
+    #
+    # elsif self<=10
+    #   change
+    # else
+    #
+    # if self > 0 && self < 5
+    #   self.join(" cents")
+    # elsif self>=5 && self<=10
+    #   self/5.join(" nickle")
+
+
+
+
+
+    # if self == 25
+    #   "1 Quarter"
+    # elsif self == 50
+    #   "2 Quarters"
+    # elsif self == 75
+    #   "3 Quarters"
+    # else self == 100
+    #   "4 Quarters"
+    # end
--- spec/coin_counter_spec.rb
+++ spec/coin_counter_spec.rb
@@ -1,8 +1,13 @@
 require("rspec")
 require("coin_counter")
+require('pry')
 
 describe("Fixnum#coin_counter") do
   it ('will return correct string for single quarter') do
     expect((25).coin_counter()).to(eq("1 Quarter"))
   end
+
+  # it ("will return string to include dimes") do
+  #   expect((10).coin_counter()).to(eq("4 Quarters"))
+  # end
 end
